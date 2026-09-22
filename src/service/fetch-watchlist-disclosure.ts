24385f9107d2acf155727a3f8b7de0f4b5313041
ryoooosk <ryosuke.iikd@gmail.com>
2026-09-22 11:38:43 +0900

    refactor: data-source/ に改名

--- src/service/fetch-watchlist-disclosure.ts
+++ src/service/fetch-watchlist-disclosure.ts
@@ -1,6 +1,6 @@
 import { loadWatchList } from '../config/watchlist.ts';
+import { fetchDisclosures } from '../data-source/tdnet.ts';
 import type { Disclosure } from '../model/disclosure.ts';
-import { fetchDisclosures } from '../source/tdnet.ts';
 
 export default async function fetchWatchlistDisclosure(
   date: string,
